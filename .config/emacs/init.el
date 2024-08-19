db139d4810c837e521b9aa7e089ada091f00b013
Jhonatan Abdala <jhonatan.jamir.abdala@gmail.com>
2024-08-18 21:49:32 -0300

    there's a long road ahead

---
+++ .config/emacs/init.el
@@ -0,0 +1,17 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(lsp-ui lsp-mode rainbow-delimiters rainbow-mode hl-todo nerd-icons-ibuffer nerd-icons-completion nerd-icons-corfu nerd-icons-dired doom-modeline doom-themes phpunit php-mode dashboard glsl-mode flycheck company org-view-mode format-all org-bullets org-modern compat)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
